44c0e720dfdde026e2233a02e7718944f501ae84
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2022-11-01 23:24:34 -0500

    Creacion de Flujo de Productos

--- VentasProducto/Negocio/Negocio/ProductoBO.cs
+++ VentasProducto/Negocio/Negocio/ProductoBO.cs
@@ -1,6 +1,43 @@
-﻿namespace Negocio.Negocio
+﻿using Repositorio;
+
+namespace Negocio.Negocio
 {
     public class ProductoBO
     {
+        /// <summary>
+        /// Creacion del Producto 
+        /// </summary>
+        /// <param name="nuevoProducto">Informacion del procuto a registrar</param>
+        /// <returns>Retorna si fue creado exitosamente</returns>
+        public bool CreacionProducto(Entidades.Producto nuevoProducto)
+        {
+            try
+            {
+                Producto producto = new Producto();
+                return new AccesoDatos().CreacionProducto(producto);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Actualizacion de Informacion del cliente 
+        /// </summary>
+        /// <param name="informacionProducto">Informacion del cliente a actualizar </param>
+        /// <returns>Retorna si fue actualizado exitosamente</returns>
+        public bool ActualizacionProducto(Entidades.Producto informacionProducto)
+        {
+            try
+            {
+                Producto producto = new Producto();
+                return new AccesoDatos().ActualizacionProducto(producto);
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
